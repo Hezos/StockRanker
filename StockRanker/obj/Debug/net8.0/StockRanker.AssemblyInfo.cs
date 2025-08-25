155a12127455cbbe9464b95e762ff53957c6af19
Nagy Bence <nbence0620@gmail.com>
2025-08-25 17:58:44 +0200

    Modified stock ranks

--- StockRanker/obj/Debug/net8.0/StockRanker.AssemblyInfo.cs
+++ StockRanker/obj/Debug/net8.0/StockRanker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockRanker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ad15a9f1f917c72fa53c12a77d0ce3bcad92e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115c563e4931a53db96f55b08e5c2f4bd096fa22")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockRanker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockRanker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
